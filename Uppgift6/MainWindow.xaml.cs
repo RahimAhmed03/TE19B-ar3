5fa23855a08e7e1a48d7ddcc473eecdfbead3609
RahimAhmed03 <rahim.ahmed@elev.ga.ntig.se>
2021-09-20 13:17:59 +0200

    Uppgift 6

---
+++ Uppgift6/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public float summa = 0;
+        public int antalClick = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void knappAdderaClick(object sender, RoutedEventArgs e)
+        {
+
+            // Lös in talet och tryparse
+            float tal;
+            if (!float.TryParse( talRuta.Text, out tal ))
+            {
+                MessageBox.Show("Tal1 Fel inmatning.\n Formatet Tex 2,5");
+            } 
+            else
+            {
+                // Räkna upp antal klick
+                antalClick++;
+                // Slriv ut nya summa
+                summaRuta.Text = $"{summa} + {tal} = {summa + tal}";
+
+                // Spara till nästa gång
+                summa = summa + tal;
+
+                // Skriv ut nya medelvärdet
+                medelRuta.Text = $"{summa} / {antalClick} = {summa / antalClick}";
+
+                // Tömma talrutan
+                talRuta.Text = "";
+            }
+
+            
+        }
+    }
+}
