4540d4e203dba852f1ec76d431a40780e4389e54
RahimAhmed03 <rahim.ahmed@elev.ga.ntig.se>
2021-09-27 13:27:52 +0200

    Labb4

--- Labb4/MainWindow.xaml.cs
+++ Labb4/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System.IO;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Labb4
 {
@@ -22,10 +11,54 @@ namespace Labb4
     public partial class MainWindow : Window
     {
 
+        static string[] rader = new string[194];
+
+
         public MainWindow()
         {
+            // Skapar fönstret och ritar ut elementen
             InitializeComponent();
 
+            rader = File.ReadAllLines("resurser/countries.csv");
+
         }
+        /// <summary>
+        /// Det som händer när användaren klickar på Sök!
+        /// </summary>                                      
+        private void ClickSök(Object sender, RoutedEventArgs e)
+        {
+
+            // Töm rutorna
+            rutaResultat.Text = "";
+            int antal = 1;
+
+            string sökterm = "";
+
+            sökterm = rutaSökterm.Text;
+
+            // Loopa igenom alla rader
+            foreach (var rad in rader)
+            {
+                // Dela upp raden
+                string[] delar = rad.Split(',');
+
+                // Plocka ut land
+                string land = delar[1];
+
+                // Plocka ut landskod
+                string landskod = delar[2];
+
+                // Hitta land som innehåller sökterm se https://www.geeksforgeeks.org/c-sharp-string-contains-method/
+                if (land.ToLower().Contains(sökterm.ToLower()))
+                {
+                    // Skriv ut matchande land och dess landskod
+                    rutaResultat.Text += $"{antal} {land}: {landskod}\n";
+                    antal++;
+                }
+                // Summering
+                rutaStatus.Text = $"Hittade {antal} länder.";
+            }
+        }
+
     }
 }
