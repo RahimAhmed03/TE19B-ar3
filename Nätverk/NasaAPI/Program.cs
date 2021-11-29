1a8e6200365caa4ff6946f77ec8a6dbcbddec122
RahimAhmed03 <rahim.ahmed@elev.ga.ntig.se>
2021-11-29 13:28:30 +0100

    Labb med API

---
+++ Nätverk/NasaAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NasaAPI
+{
+    class Bild
+    {
+        public string 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hämta nyaste bilden från NASA");
+        }
+    }
+}
--- Nätverk/StarWarsAPI/Program.cs
+++ Nätverk/StarWarsAPI/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using RestSharp;
+using System.Text.Json;
+using System.Collections.Generic;
+
 
 namespace StarWarsAPI
 {
+    class karaktär
+    {
+        public string name {get; set;}
+    }
+    class karaktärer
+    {
+        public List<karaktär> results {get; set;}
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hämta alla karaktärer ifrån Star Wars");
+
+            // Hämta karaktärer från  swapi.dev
+            string url =  "https://swapi.dev/api";
+            var client = new RestClient(url);
+            var request = new RestRequest("people", DataFormat.Json);
+            var response = client.Get(request);
+
+            // Avkoda JSON-svaret
+            karaktärer lista = new karaktärer();
+            lista = JsonSerializer.Deserialize<karaktärer>(response.Content);
+
+            // SKriv ut alla karakäterer
+            if (lista != null)
+            {
+                foreach (var item in lista.results)
+                {
+                    Console.WriteLine(item.name);
+                }
+                
+            }
         }
     }
 }
