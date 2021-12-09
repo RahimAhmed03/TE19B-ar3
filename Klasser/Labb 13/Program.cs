aa86cd27b1cb8457d0aeff002cb75393b5739d82
RahimAhmed03 <rahim.ahmed@elev.ga.ntig.se>
2021-12-09 09:55:15 +0100

    Tränat på API med bilder

---
+++ Klasser/Labb 13/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Collections.Generic;
+using System;
+
+namespace Labb_13
+{
+    class Namn
+    {
+        public string Förnamn {get; set;}
+        public string Efternamn {get; set;}
+
+        public void VisaNamn()
+        {
+            Console.WriteLine($"{Förnamn} + {Efternamn}");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program för att skriva in namn");
+
+            List<Namn> lista = new List<Namn>();
+
+            while (true)
+            {
+                
+                Namn objekt = new Namn();
+
+                // Ange Förnamn
+                Console.Write("Ange ditt Förnamn: ");
+                objekt.Förnamn = Console.ReadLine();
+
+                // Avbryt om namn är tomt
+                if (objekt.Förnamn == "")
+                {
+                    break;
+                }
+
+                Console.Write("Ange ditt Efternamn: ");
+                objekt.Efternamn = Console.ReadLine();
+
+                lista.Add(objekt);
+            }
+            foreach (var namn in lista)
+            {
+                namn.VisaNamn();
+            }
+
+
+            
+        }
+    }
+}
