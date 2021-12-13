fe35e18da760236c36d38ebd8147731c128a4516
RahimAhmed03 <rahim.ahmed@elev.ga.ntig.se>
2021-12-13 13:25:01 +0100

    Labb 13 & 14

--- Klasser/Labb 13/Program.cs
+++ Klasser/Labb 13/Program.cs
@@ -4,29 +4,74 @@ using System;
 
 namespace Labb_13
 {
-    class Namn
+    class Person
     {
         public string Förnamn {get; set;}
         public string Efternamn {get; set;}
+        public DateTime Födelsedag {get; set;}
+        public float Vikt {get; set;}
+        public float Höjd {get; set;}
 
         public void VisaNamn()
         {
-            Console.WriteLine($"{Förnamn} + {Efternamn}");
+            Console.WriteLine($" Ditt namn är {Förnamn} {Efternamn}");
         }
+
+        public void VisaÅlder()
+        {
+            int ålder = DateTime.Now.Year - Födelsedag.Year;
+            Console.WriteLine($"Ditt ålder är {ålder}");
+        }
+
+        public float BMI()
+        {
+            return Vikt / (Höjd * Höjd);
+        }
+
 
     }
     class Program
     {
         static void Main(string[] args)
         {
+            DateTime datum = DateTime.Now;
+            Console.WriteLine($"Dagens datum är {datum}");
+            Console.WriteLine($"Vi är i månad {datum.Month}");
+            Console.WriteLine($"Vi är i dag {datum.Day}");
+            Console.WriteLine($"Vi är i dag {datum.Hour}:{datum.Minute}");
+        
+            // Formatera datumet
+            Console.WriteLine($"Dagens datum är {datum.ToString("dddd")}");
+            Console.WriteLine($"Dagens datum är {datum.ToString("hh:mm")}");
+
+            // Om 40 dagar skall arbetet vara klart, vilken dag är det?
+            DateTime deadline = datum.AddDays(40);
+            Console.WriteLine($"Arbetet skall vara klart {datum.ToString("dddd dd/MM-YY")}");
+
+            // Be användare om födeksedatum
+            Console.Write("När är du född? (yy-mm-dd) ");
+            DateTime födelsedatum = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine(födelsedatum);
+
+            // Berätta hur gammal användaren är?
+            Console.WriteLine($"Din ålder är {datum.Year - födelsedatum.Year}");
+            Console.WriteLine($"Din ålder är {GetAge(födelsedatum)}");
+
+            // Vad är DateTime.Ticks?
+            Console.WriteLine(födelsedatum.Ticks);
+
+            Console.Write("Hur mycket väger du? ");
+            Console.ReadLine();
+
+
             Console.WriteLine("Program för att skriva in namn");
 
-            List<Namn> lista = new List<Namn>();
+            List<Person> lista = new List<Person>();
 
             while (true)
             {
                 
-                Namn objekt = new Namn();
+                Person objekt = new Person();
 
                 // Ange Förnamn
                 Console.Write("Ange ditt Förnamn: ");
@@ -51,5 +96,14 @@ namespace Labb_13
 
             
         }
+
+        static public int GetAge(DateTime datum)
+        {
+            double age = 0;
+            age = DateTime.Now.Subtract(datum).Days;
+            age = age / 365.25;
+            return (int)Math.Floor(age);
+        }
+
     }
 }
---
+++ Klasser/Labb 14/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labb_14
+{
+
+    class Bok
+    {
+        public string Titel { get; set; }
+        public string Författare { get; set; }
+        public int AntalSidor { get; set; }
+
+        public void Skrivut()
+        {
+            Console.WriteLine($"Boken heter {Titel}, författaren är {Författare} och antal sidor är {AntalSidor}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bok> lista = new List<Bok>();
+
+            while (true)
+            {
+                Bok objekt = new Bok();
+
+                // Ange Titel
+                Console.Write("Ange bokens Titel: ");
+                objekt.Titel = Console.ReadLine();
+
+                // Avbryt om namn är tomt
+                if (objekt.Titel == "")
+                {
+                    break;
+                }
+
+                // Ange Författare
+                Console.Write("Ange namnet på författare: ");
+                objekt.Författare = Console.ReadLine();
+
+                // Ange AntalSidor
+                Console.Write("Ange Antalet sidor boken har: ");
+                objekt.AntalSidor = Console.ReadLine();
+
+                lista.Add(objekt);
+            }
+            foreach (var bok in lista)
+            {
+                bok.Skrivut();
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
