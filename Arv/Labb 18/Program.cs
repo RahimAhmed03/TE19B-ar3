04388708314a6305a6dde3911706b62692f7aff5
RahimAhmed03 <rahim.ahmed@elev.ga.ntig.se>
2022-02-17 10:44:59 +0100

    Labb 18

---
+++ Arv/Labb 18/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Labb_18
+{
+    public class Creature
+    {
+        public int Health { get; set; }
+        public int Shield { get; set; }
+        protected string Name { get; set;}
+
+        public Creature(string name)
+        {
+            Name = name;
+        }
+
+        public string GetInfo()
+        {
+            
+        }
+    }
+
+    class Knight : Creature
+    {
+        protected string Weapon { get; set; }
+
+        public Knight(string name, string weapon)
+        {
+            Name = name;
+
+        }
+        public void Attack()
+        {
+
+        }
+    }
+
+    class Dragon : Creature
+    {
+        protected string color { get; set; }
+
+        public Dragon(string name, string color)
+        {
+            Name = name;
+        }
+        
+        public void Attack()
+        {
+
+        }
+    }
+}
