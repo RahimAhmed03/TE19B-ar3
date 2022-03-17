182cb0df8a85535a67f19d28d441528c1cc91d90
RahimAhmed03 <rahim.ahmed@elev.ga.ntig.se>
2022-03-17 10:56:15 +0100

    OOP Arv

---
+++ Arv/OOP Arv/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Arv
+{
+    class Program
+    {
+        class Creature
+        {
+            public int Power { get; set; }
+            public int Toughness { get; set; }
+            public string Type { get; set; }
+
+            public Creature(string type, int power, int toughness)
+            {
+                Type = type;
+                Power = power;
+                Toughness = toughness;
+            }
+
+            public virtual void Attack()
+            {
+                Console.WriteLine(Type + " attacks with power " + Power);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<Creature> creatures = new List<Creature>();
+            creatures.Add(new Creature("Knight", 5, 5));
+            creatures.Add(new Creature("Wizard", 7, 3));
+
+            foreach(var c in creatures)
+                c.Attack();
+
+
+            Console.ReadKey();
+        }
+    }
+}
